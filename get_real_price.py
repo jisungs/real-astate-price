5222eb749ed2e4b8f9ef6a46861248a92212a80a
Jisung Smart <jisung.smart@gmail.com>
2024-09-08 16:49:50 +0900

    실거래가 검색까지 완료

--- get_real_price.py
+++ get_real_price.py
@@ -1,25 +1,53 @@
-
-
+import os
 from PublicDataReader import TransactionPrice
 import PublicDataReader as pdr
 import pandas as pd
 
-service_key = "API_KEY"
+from dotenv import load_dotenv
+load_dotenv()
+
+service_key = os.environ.get("API_KEY")
 
 api = TransactionPrice(service_key)
 
 #시군구 코드 조회
 
-def sigungu_code(sido_name, sigungu_name):
-    code = pdr.code_bdong()
-    df = code.loc[(code['시도명'].str.contains(sido_name)) & (code['시군구명'].str.contains(sigungu_name)) &
-         (code['읍면동명']=='')]
-    result = df['시군구코드']
-    print(result)
-    if len(result) >= 2:
-       result.iloc[1]
+def sigungu_code(addr):
+    sido_name = addr[0]
+    
+    address_link  = {
+        '경남' : '경상남도',
+        '충북' : '충청북도',
+        '충남' : '충청남도',
+        '전남' : '전라남도',
+        '전북' : '전라북도',
+        '경북' : '경상북도',
+    }
+
+    if sido_name in address_link:
+        sido_name = address_link[sido_name]
+        sigungu_name = addr[2]
+    
+
+        code = pdr.code_bdong()
+        df = code.loc[(code['시도명'].str.contains(sido_name)) & (code['시군구명'].str.contains(sigungu_name)) &
+            (code['읍면동명']=='')]
+        result = df['시군구코드'].tolist()
+    
     else:
-        result.iloc[0]
+        sido_name = addr[0]
+        sigungu_name = addr[1]
+        code = pdr.code_bdong()
+        df = code.loc[(code['시도명'].str.contains(sido_name)) & (code['시군구명'].str.contains(sigungu_name)) &
+            (code['읍면동명']=='')]
+        result = df['시군구코드'].tolist()
+
+    
+    if len(result) >= 2:
+        return result[1]
+    else:
+        return result[0]
+     
     
 def search_apt_price(type, sigungu_code, start_year_month, end_year_month):
     # 기간 내 조회
@@ -34,8 +62,12 @@ def search_apt_price(type, sigungu_code, start_year_month, end_year_month):
     return df
 
 
-sigungu_code = sigungu_code('부산', '해운대구')
-print(sigungu_code)
 
-# apt_price = search_apt_price('아파트',sigungu_code,'202401','202408')
-# print(apt_price.head())
+if __name__ == "__main__":
+    sigungu_code = sigungu_code('부산', '강서구')
+
+    print(sigungu_code)
+
+    apt_price = search_apt_price('아파트',sigungu_code,'202401','202408')
+    print(apt_price.head())
+    pass
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template, request, redirect,url_for
 
+from get_real_price import sigungu_code , search_apt_price
+
+
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods = ["GET","POST"])
 def index():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        main_addr = data['address'].split()
+        if main_addr:        
+            code = sigungu_code(main_addr)
+            result = search_apt_price('아파트',code,'202401','202408')
+            
+            table_html = result.to_html(classes='table table-striped', index=False)
+
+            return render_template('index.html', table_html=table_html)
+
+
+        
+
     return render_template("index.html")
 
 
